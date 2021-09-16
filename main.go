ae2b199688b006cdd1373082df1d56755c09db3e
Mili Vishwakarma <milivish06@gmail.com>
2021-09-15 19:43:20 -0500

    make anaytics end points as /api/v1/analytics/top/{limit}, /api/v1/analytics/{shortURL}?param

--- main.go
+++ main.go
@@ -21,6 +21,8 @@ type urlShort struct {
 
 var cache lru.LRUCache
 const apiURL = "/api/v1/url/"
+const topAnalyticsURL = "/api/v1/analytics/top"
+const timeAnalyticsURL = "/api/v1/analytics/"
 
 func main() {
         
@@ -46,6 +48,9 @@ func main() {
 
     urlAnalyticsHandler := http.HandlerFunc(urlAnalyticsOperations)
     http.Handle("/api/v1/analytics/", urlAnalyticsHandler)
+
+    topUrlAnalyticsHandler := http.HandlerFunc(topUrlAnalyticsOperations)
+    http.Handle("/api/v1/analytics/top/", topUrlAnalyticsHandler)
 
     fs := http.FileServer(http.Dir("static/"))
     http.Handle("/", fs)
@@ -113,7 +118,7 @@ func urlShortenAPIOperations(w http.ResponseWriter, r *http.Request) {
 
         if len(shortURL) < 1 {
             log.Println("Url param 'shortURL' is missing")
-            setErrorResponse(w,"short-url-missing", "No short URL provided", http.)
+            setErrorResponse(w,"short-url-missing", "No short URL provided", http.StatusNotFound)
             return
         }
 
@@ -191,35 +196,42 @@ func urlShortenAPIOperations(w http.ResponseWriter, r *http.Request) {
 }
 
 // Handler for analytics requests
-// GET /analytics
-func urlAnalyticsOperations(w http.ResponseWriter, r *http.Request) {
-    query := r.URL.Query()
-    paramURL := query["shortURL"]
-    paramHour := query["hours"]
-    paramDay := query["days"]
-    paramTop := query["top"]
 
-    // If no parameters are specified, provide the usage
-    if(len(paramURL) == 0 && len(paramHour) == 0 && len(paramDay) == 0 && len(paramTop) == 0) {
-        setErrorResponse(w, "no-params-found", "Usage: /analytics?top=n, /analytics?shortURL=url&hours=n, /analytics?shortURL=url&days=n", http.StatusBadRequest)
-        return
+// GET api/v1/analytics/top
+func topUrlAnalyticsOperations(w http.ResponseWriter, r *http.Request) {
+    paramTop := strings.TrimPrefix(r.URL.Path, topAnalyticsURL)
+    paramTop = strings.TrimPrefix(paramTop, "/")
+
+    if len(paramTop) == 0 {
+        paramTop = "5"
     }
 
-    // top n URLs visited
-    if(len(paramTop) != 0) {
-        urlCount, err := dbService.Top(paramTop[0])
+    urlCount, err := dbService.Top(paramTop)
 
-        if err != nil {
-            log.Println("urlAnalyticsOperations: Failed to get top five most visited URLs")
-            setErrorResponse(w, err.Error(), "Failed to get top " + paramTop[0] + "visited URLs", http.StatusInternalServerError)
-        }
+    if err != nil {
+        log.Println("urlAnalyticsOperations: Failed to get top five most visited URLs")
+        setErrorResponse(w, err.Error(), "Failed to get top " + paramTop + "visited URLs", http.StatusInternalServerError)
+        return
+    }
 
     w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(http.StatusOK)
     jsonResponse, _ := json.Marshal(urlCount)
     w.Write(jsonResponse)
     return
-    } 
+}
+
+func urlAnalyticsOperations(w http.ResponseWriter, r *http.Request) {
+    query := r.URL.Query()
+    paramURL := strings.TrimPrefix(r.URL.Path, timeAnalyticsURL)
+    paramHour := query["hours"]
+    paramDay := query["days"]
+
+    // If no parameters are specified, provide the usage
+    if len(paramURL) == 0 && len(paramHour) == 0 && len(paramDay) == 0 {
+        setErrorResponse(w, "no-params-found", "Usage: /analytics?top=n, /analytics?shortURL=url&hours=n, /analytics?shortURL=url&days=n", http.StatusBadRequest)
+        return
+    }
 
     if len(paramURL) < 1 {
         log.Println("Url param 'shortURL' is missing")
@@ -229,14 +241,14 @@ func urlAnalyticsOperations(w http.ResponseWriter, r *http.Request) {
 
     // get URL count by hours
     if len(paramHour) != 0 {
-        urlCountHour, err := dbService.GetURLCountPastnHours(paramURL[0], paramHour[0])
+        urlCountHour, err := dbService.GetURLCountPastnHours(paramURL, paramHour[0])
         if err != nil {
             setErrorResponse(w, err.Error(), "Failed to URL calls by hour", http.StatusInternalServerError)
             return
         }
 
         response := make(map[string]int)
-        response[paramURL[0]] = urlCountHour
+        response[paramURL] = urlCountHour
 
         setResponseForAnalytics(w, response, http.StatusOK)
         return
@@ -244,14 +256,14 @@ func urlAnalyticsOperations(w http.ResponseWriter, r *http.Request) {
 
     // get URL count by days
     if len(paramDay) != 0 {
-        urlCountDay, err := dbService.GetURLCountPastnDays(paramURL[0], paramDay[0])
+        urlCountDay, err := dbService.GetURLCountPastnDays(paramURL, paramDay[0])
         if err != nil {
             setErrorResponse(w, err.Error(), "Failed to URL calls by hour", http.StatusInternalServerError)
             return
         }
 
         response := make(map[string]int)
-        response[paramURL[0]] = urlCountDay
+        response[paramURL] = urlCountDay
 
         setResponseForAnalytics(w, response, http.StatusOK)
         return
